5d914a5f918134005b9274de472b93cd10119fa7
yami2200 <TheYami22@gmail.com>
2023-05-04 16:35:23 +0200

    Improved UI for waiting room page.

--- lib/room_page.dart
+++ lib/room_page.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_p2p_minigames/utils/GameParty.dart';
 import 'package:flutter_p2p_minigames/utils/PlayerInfo.dart';
+import 'package:flutter_p2p_minigames/widgets/PlayerInfoRow.dart';
 import 'package:go_router/go_router.dart';
 
 import 'main.dart';
@@ -18,28 +20,41 @@ class RoomPage extends StatefulWidget {
 }
 
 class _RoomPageState extends State<RoomPage> {
-  List<PlayerInfo> playerList = [];
+  final Future<PlayerInfo> _playerInfo = Storage().getPlayerInfo();
+  final Completer<PlayerInfo> _myOpponentCompleter = Completer<PlayerInfo>();
+  Future<PlayerInfo> get _opponentInfo => _myOpponentCompleter.future;
 
   @override
   void initState() {
     super.initState();
-    addPlayerInfo();
     PeerToPeer().clearP2PConnectionListener();
     PeerToPeer().clearP2PPeersChangeListener();
     GameParty().connection!.addClientMessageListener((message) {
       log("Received message: $message");
       if(message.type == EventType.PLAYER_JOINED.text){
         PlayerInfo newPlayer = PlayerInfo.fromJson(jsonDecode(message.data));
-        setState(() {
-          playerList.add(newPlayer);
-        });
+        _myOpponentCompleter.complete(newPlayer);
       } else if(message.type == EventType.START_GAME.text){
-        GameParty().startGame(playerList);
-        BuildContext? ctx = MyApp.router.routerDelegate.navigatorKey.currentContext;
-        ctx!.go("/hub");
+        startGame();
       }
     });
     sendPlayerInfoToHost();
+  }
+
+  void startGame() async {
+    final futures = [
+      _playerInfo,
+      _opponentInfo,
+    ];
+    final results = await Future.wait(futures);
+    List<PlayerInfo> playerList = results;
+    GameParty().startGame(playerList);
+    openHub();
+  }
+
+  void openHub(){
+    BuildContext? ctx = MyApp.router.routerDelegate.navigatorKey.currentContext;
+    ctx!.go("/hub");
   }
 
   void sendPlayerInfoToHost() async {
@@ -47,69 +62,66 @@ class _RoomPageState extends State<RoomPage> {
     GameParty().connection!.sendMessageToServer(jsonEncode(EventData(EventType.PLAYER_JOINED.text, jsonEncode(player))));
   }
 
-  void addPlayerInfo() async {
-    PlayerInfo player = await Storage().getPlayerInfo();
-    setState(() {
-      playerList.add(player);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Player List'),
+        title: const Text('Waiting room',
+            style: TextStyle(
+              fontFamily: 'SuperBubble',
+              fontSize: 24,
+            )),
+        backgroundColor: const Color.fromRGBO(68, 71, 50, 1.0),
       ),
-      body: Column(
+      body: Container(
+        decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/ui/background_create.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child:Padding(
+      padding: const EdgeInsets.all(16.0),
+      child:Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Expanded(
-            child: Center(
-              child: ListView.builder(
-                itemCount: playerList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final player = playerList[index];
-                  final username = player.username;
-                  final tag = player.tag;
-                  final avatar = player.avatar;
-
-                  return Padding(
-                    padding: EdgeInsets.symmetric(vertical: 8.0),
-                    child: Row(
+          Center(
+              child: Card(
+                color: const Color.fromRGBO(254, 223, 176, 0.8),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child:Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        CircleAvatar(
-                          backgroundImage: AssetImage('assets/avatars/$avatar'),
-                          backgroundColor: Colors.transparent,
+                        const Text(
+                          'Player list :',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'SuperBubble',
+                          ),
                         ),
-                        SizedBox(width: 16),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tag,
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            Text(
-                              username,
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                },
+                        const SizedBox(height: 16),
+                        PlayerInfoRow(playerInfo: _playerInfo),
+                        PlayerInfoRow(playerInfo: _opponentInfo),
+                      ]),
+                ),
               ),
             ),
+          const Spacer(),
+          const Center(
+              child: Text(
+                'Waiting for host to launch the game...',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25,
+                    fontFamily: 'SuperBubble',
+                    color: Colors.black),
+              ),
           ),
-          Text(
-            'Waiting for host to launch the game...',
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 16),
+          const SizedBox(height: 16),
         ],
+      ),
+      ),
       ),
     );
   }
