94b34681dd280ed95ca82e45c920c2ef9d6c1840
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-05-09 17:02:58 +0200

    feat: improve multiplayer

--- lib/utils/GameParty.dart
+++ lib/utils/GameParty.dart
@@ -24,13 +24,13 @@ class GameParty {
   Connection? get connection => _connection;
 
   GameParty._internal() {
-    // gameList.putIfAbsent("CapyQuiz", () => GameInfo("CapyQuiz", "Participate to a quiz about Capybaras. Be the first with the most good answers.", "/quiz/c"));
-    // gameList.putIfAbsent("FaceGuess", () => GameInfo("FaceGuess", "Remember the face on the screen. Try to recreate it before your opponent !", "/faceguess/c"));
-    // gameList.putIfAbsent("Safe Landing", () => GameInfo("Safe Landing", "Try to land when the counter hit 0 seconds.", "/safe_landing/c"));
-    // gameList.putIfAbsent("Fruits Slash", () => GameInfo("Fruits Slash", "Slash these fruits!", "/fruits_slash/c"));
-    // gameList.putIfAbsent("Choose the good side", () => GameInfo("Choose the good side", "Choose one side of the platform. Pray to not die before your opponent!", "/choosegoodside/c"));
-    // gameList.putIfAbsent("Eat That Cheese", () => GameInfo("Eat That Cheese", "Eat all that cheese.", "/eat_that_cheese/c"));
-    // gameList.putIfAbsent("Train Tally", () => GameInfo("Train Tally", "Count the passengers of the train passing by!", "/traintally/c"));
+    gameList.putIfAbsent("CapyQuiz", () => GameInfo("CapyQuiz", "Participate to a quiz about Capybaras. Be the first with the most good answers.", "/quiz/c"));
+    gameList.putIfAbsent("FaceGuess", () => GameInfo("FaceGuess", "Remember the face on the screen. Try to recreate it before your opponent !", "/faceguess/c"));
+    gameList.putIfAbsent("Safe Landing", () => GameInfo("Safe Landing", "Try to land when the counter hit 0 seconds.", "/safe_landing/c"));
+    gameList.putIfAbsent("Fruits Slash", () => GameInfo("Fruits Slash", "Slash these fruits!", "/fruits_slash/c"));
+    gameList.putIfAbsent("Choose the good side", () => GameInfo("Choose the good side", "Choose one side of the platform. Pray to not die before your opponent!", "/choosegoodside/c"));
+    gameList.putIfAbsent("Eat That Cheese", () => GameInfo("Eat That Cheese", "Eat all that cheese.", "/eat_that_cheese/c"));
+    gameList.putIfAbsent("Train Tally", () => GameInfo("Train Tally", "Count the passengers of the train passing by!", "/traintally/c"));
     gameList.putIfAbsent("Arrow Swiping", () => GameInfo("Arrow Swiping", "Swipe in the direction of the arrow!", "/arrow_swiping/c"));
 
     loadPlayer();
